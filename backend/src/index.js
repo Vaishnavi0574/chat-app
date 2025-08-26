88c08dc6fabdf6bc5d8adc83aa938dd0de9535c8
Vaishnavi0574 <vaishnavisingh0574@gmail.com>
2025-08-26 21:02:26 +0530

    project

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json({limit:"10mb"}));
 
 //for connecting frontend and backend as have different ports
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174'],
+  origin: ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
