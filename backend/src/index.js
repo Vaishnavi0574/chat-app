0b763f447244e3701b6155d601d7d585238ccb42
Vaishnavi0574 <vaishnavisingh0574@gmail.com>
2025-08-27 09:37:36 +0530

    Fix: corrected axios baseURL to point to backend API in dev mode

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,7 +20,6 @@ app.use(express.json({limit:"10mb"}));
 app.use(cors({
   origin: [
     "http://localhost:5173", 
-    
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -3,7 +3,9 @@ import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 import {io } from "socket.io-client"
 
-const BASE_URL=import.meta.env.Mode==="development"? 'http://localhost:3000/api':"/";
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:3000/api"
+  : "/api";
 export const useAuthStore = create((set,get) => ({
     authUser: null,
     isSigningUp: false,  
